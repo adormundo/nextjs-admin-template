3b629a595543f5b8abf39905ed03034561c7adef
Alvaro Dormundo <adormundo@gmail.com>
2023-01-13 12:44:37 -0300

    Context API #2

--- src/data/context/AppContext.tsx
+++ src/data/context/AppContext.tsx
@@ -1,14 +1,26 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
-const AppContext = createContext({
-  nome: '',
-});
+type Tema = 'dark' | '';
+
+interface AppContextProps {
+  tema?: Tema;
+  alternarTema?: () => void;
+}
+
+const AppContext = createContext<AppContextProps>({});
 
 export function AppProvider(props: any) {
+  const [tema, setTema] = useState<Tema>('dark');
+
+  function alternarTema() {
+    setTema(tema === '' ? 'dark' : '');
+  }
+
   return (
     <AppContext.Provider
       value={{
-        nome: 'oxe',
+        tema,
+        alternarTema,
       }}
     >
       {props.children}
