b32a13df141e8bfb82a6737dfac1fc5d06128e61
Alvaro Dormundo <adormundo@gmail.com>
2023-01-15 20:33:45 -0300

    Autenticação com Google

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -4,36 +4,43 @@ import firebase from '../../firebase/config';
 import Usuario from '../../model/Usuario';
 import { useState } from 'react';
 
-interface AuthContextProps{
-  usuairo?: Usuario
-  loginGoogle?: () => Promise<void>
+interface AuthContextProps {
+  usuairo?: Usuario;
+  loginGoogle?: () => Promise<void>;
 }
 
-export const AuthContext = createContext<AuthContextProps>({})
+export const AuthContext = createContext<AuthContextProps>({});
 
-async function usuarioNormalizado(usuarioFirebase: firebase.User): Promise<Usuario> {
-  const token = await usuarioFirebase.getIdToken()
+async function usuarioNormalizado(usuarioFirebase: any): Promise<Usuario> {
+  const token = await usuarioFirebase.getIdToken();
   return {
     uid: usuarioFirebase.uid,
     nome: usuarioFirebase.displayName || '',
     email: usuarioFirebase?.email || '',
     token,
     provedor: usuarioFirebase.providerData[0]?.providerId || '',
-    imagemUrl: usuarioFirebase?.photoURL || ''
-  }
+    imagemUrl: usuarioFirebase?.photoURL || '',
+  };
 }
 
 export function AuthProvider(props: any) {
-  const [usuairo, setUsuario] = useState<Usuario>()
+  const [usuairo, setUsuario] = useState<Usuario>();
 
   async function loginGoogle() {
-    console.log('Login google...')
-    Router.push('/')
+    const resp = await firebase
+      .auth()
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider());
+
+    if (resp.user?.email) {
+      const usuario = await usuarioNormalizado(resp.user);
+      setUsuario(usuario);
+      Router.push('/');
+    }
   }
 
-  return(
-    <AuthContext.Provider value={{usuairo, loginGoogle}}>
+  return (
+    <AuthContext.Provider value={{ usuairo, loginGoogle }}>
       {props.children}
     </AuthContext.Provider>
-  )
+  );
 }
