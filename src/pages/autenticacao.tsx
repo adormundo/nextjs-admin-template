6870ab7ba199f4bec191fc1d4a7cca0f39572cca
Alvaro Dormundo <adormundo@gmail.com>
2023-01-15 15:42:02 -0300

    Tela Autenticação #04

--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -1,17 +1,27 @@
 import { useState } from 'react';
 import AuthInput from '../auth/AuthInput';
 import { FcGoogle } from 'react-icons/fc';
+import { IconeAtencao } from '../components/icons';
 
 export default function Autenticacao() {
   const [modo, setModo] = useState<'login' | 'cadastro'>('login');
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
+  const [erro, setErro] = useState(null);
+
+  function exibirErro(msg: any, tempoEmSegundos = 8) {
+    setErro(msg)
+    setTimeout(() => setErro(null), tempoEmSegundos * 1000)
+  }
 
   function submeter() {
     if (modo === 'login') {
       console.log('login');
+      exibirErro('Ocorreu um erro no login!')
     } else {
       console.log('cadastrar');
+      exibirErro('Ocorreu um erro no Cadastro!')
+
     }
   }
 
@@ -30,6 +40,18 @@ export default function Autenticacao() {
             ? 'Entre com a sua conta'
             : 'Cadastre-se na plataforma'}
         </h1>
+
+        {erro ? (
+          <div
+            className={`flex items-center bg-red-400 text-white py-3 px-5 my-2 border border-red-700 rounded-lg`}
+          >
+            {IconeAtencao()}
+            <span className={`ml-3`}>{erro}</span>
+          </div>
+        ) : (
+          false
+        )}
+
         <AuthInput
           tipo="email"
           label="Email"
@@ -58,7 +80,7 @@ export default function Autenticacao() {
 
         <button
           onClick={submeter}
-          className={`flex gap-2 justify-center
+          className={`flex gap-2 items-center justify-center
           w-full bg-red-400 hover:bg-red-300
           text-white rounded-lg px-4 py-3`}
         >
@@ -68,25 +90,31 @@ export default function Autenticacao() {
 
         {modo === 'login' ? (
           <p className={`mt-8`}>
-            Novo por aqui? 
+            Novo por aqui?
             <a
               onClick={() => setModo('cadastro')}
               className={`
                 text-blue-500 hover:text-blue-700 font-semibold
                 cursor-pointer
               `}
-            > Crie uma conta gratuitamente
+            >
+              {' '}
+              Crie uma conta gratuitamente
             </a>
           </p>
         ) : (
-          <p className={`mt-8`}> Já faz parte da nossa comunidade?
+          <p className={`mt-8`}>
+            {' '}
+            Já faz parte da nossa comunidade?
             <a
               onClick={() => setModo('login')}
               className={`
                 text-blue-500 hover:text-blue-700 font-semibold
                 cursor-pointer
               `}
-            > Entre com a suas credenciais
+            >
+              {' '}
+              Entre com a suas credenciais
             </a>
           </p>
         )}
